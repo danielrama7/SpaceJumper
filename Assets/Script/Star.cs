9dea08f221ad864cfaeb0af47157c7623919c293
danielrama7 <danielrama017@gmail.com>
2021-05-04 23:19:59 +0700

    week2

---
+++ Assets/Script/Star.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Star : MonoBehaviour
+{
+    public GameObject scoreBox;
+    public AudioSource collectSound;
+    
+    void OnTriggerEnter()
+    {
+        Score.currentScore += 1000;
+        collectSound.Play();
+        Destroy(gameObject);
+    }
+}
